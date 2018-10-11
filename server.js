46e4973a10b862539a0e6a206b4a3ec962495368
monojit18 <datta.monojit@gmail.com>
2018-10-11 12:29:51 +0530

    v1.76

--- server.js
+++ server.js
@@ -22,14 +22,14 @@ express.use(BodyParser.urlencoded
 express.get("/fetch", (request, response) =>
 {
 
-    response.send("HelloJSApp - Fetch1.75\n" + "\n");
+    response.send("HelloJSApp - Fetch1.76\n" + "\n");
     
 });
 
 express.post("/upload", (request, response) =>
 {
 
-    response.send("HelloJSApp - Upload1.75\n" + JSON.stringify(request.body) + "\n");
+    response.send("HelloJSApp - Upload1.76\n" + JSON.stringify(request.body) + "\n");
     
 });
 
